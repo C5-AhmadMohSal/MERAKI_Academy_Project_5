65c88800dae59a4ef645a0555b9d21c7cb41ee1c
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-06-02 11:25:45 +0300

    auth:authReducer

--- frontend/src/redux/reducers/auth/index.js
+++ frontend/src/redux/reducers/auth/index.js
@@ -1,2 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+/* A reducer that is used to store the
+ token and isLoggedIn state. */
+export const auth = createSlice({
+  name: "auth",
+  initialState: {
+    token:localStorage.getItem("token") || "",
+    isLoggedIn:localStorage.getItem("token")?true:false
+  },
+  reducers: {
+    setlogin: (state, action) => {
+        state.token=action.token;
+        state.isLoggedIn = true;
+        localStorage.setItem('token',action.token);
+    },
+    logout: (state, action) => {
+        state.token = "";
+        state.isLoggedIn = false;
+        localStorage.clear();
+      },
+   
+  },
+});
+
+export const { setlogin, logout } = auth.actions;
+
+export default auth.reducer;
